c0e31aecd26229dd0382f375a3b99b49c1256ccb
Benja <benjaminokumu12@gmail.com>
2022-05-19 18:07:48 +0300

    nodejs

--- lesson1/stream.js
+++ lesson1/stream.js
@@ -3,10 +3,20 @@ const server = require("http").createServer();
 
 server.on("request", (req, res) => {
     // Solution 1
-    fs.readFile("Test-file.txt", (err, data) => {
-        if (err) console.log(err);
-        res.end(data)
-    }); 
+    // fs.readFile("Test-file.txt", (err, data) => {
+    //     if (err) console.log(err);
+    //     res.end(data)
+    // }); 
+
+
+    // solution2
+    const readable = fs.createReadStream('Test-file.txt')
+    readable.on('data', chunk => {
+        res.write(chunk);
+    })
+    readable.on("end", () => {
+        res.end();
+    })
 });
 
 server.listen(8000, "127.0.0.1", () => {

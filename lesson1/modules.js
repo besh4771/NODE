f81570dbdff29ca9c376951b8dbd76d2b0e13019
Benja <benjaminokumu12@gmail.com>
2022-05-19 18:58:24 +0300

    nodejs

--- lesson1/modules.js
+++ lesson1/modules.js
@@ -5,3 +5,16 @@
 const C = require('./test-module1')
 const Calc1 = new C();
 console.log(Calc1.add(2, 5));
+
+
+// exports
+// const calc2 = require('./test-module2');
+const { add, multiply, divide } = require('./test-module2');
+console.log(divide(2, 5))
+
+
+// Caching
+require('./test-module3')();
+require('./test-module3')();
+require('./test-module3')();
+require('./test-module3')();
